e797dc4c5b75d62d0d998fbe998b61aa79734981
Johnny Halife <johnny@mural.ly>
2014-05-23 01:25:55 -0700

    published the first task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,27 @@ module.exports = function(grunt) {
 			release: {
 				files: [ { src: './**', dest: './dist/' } ]
 			}
+		},
+		version: {
+			release: {
+				options: {
+					buildNumber: "1.0." + (+new Date())
+				}
+			},
+			dev: {
+				options: {
+					buildNumber: function() {
+						var package = JSON.parse(grunt.file.read('./package.json'));
+						var chunks = package.version.split('.');
+						chunks[1] = parseInt(chunks[1], 10) + 1;
+						return chunks.join('.') + '-pre';
+					}()
+				}
+			}
 		}
 	});
 
 	require('load-grunt-tasks')(grunt);
 
-	grunt.registerTask('default', ['clean', 'copy']);
+	grunt.registerTask('default', ['clean', 'copy', 'version:release']);
 };
